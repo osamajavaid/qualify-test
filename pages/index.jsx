8133d912baf57a2c56475e8a8be3deb687c482b1
osamajavaid <18pwcse1638@uetpeshawar.edu.pk>
2023-06-13 01:30:02 +0500

    all 10 question solved

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,20 +3,77 @@ import { useState } from "react";
 import { useQuery } from "react-query";
 
 const home = () => {
-    const [isData, setIsData] = useState([]);
+    const [apiData, setApiData] = useState([]);
 
     const { isLoading, error, data } = useQuery('portfolio', () =>
         axios.get('api/Data')
-            .then(({ data }) => setIsData(data))
+            .then(({ data }) => setApiData(data[0]))
             .catch(error => console.error('Error fetching testimonials:', error)))
 
 
     if (isLoading) return <p>Loading...</p>
     if (error) return <p>Error :(</p>
-    console.log(isData)
+    // console.log(apiData)
+
+    console.log("Question 1: ", apiData?.DataProvider?.global?.positions?.filter(obj => obj.type === 'DIRECTOR').length)
+    console.log("Question 2: ", apiData?.DataProvider?.global?.positions?.filter(obj => obj.type === 'SHAREHOLDER').length)
+    console.log("Question 3: ", apiData?.DataProvider?.global?.positions?.map(position => position.profile.name))
+    console.log("Question 4: ", 'Based on the provided JSON object, "Product B" is categorized as "Hardware".')
+    console.log("Question 4B: ", 'Yes, based on the provided JSON object, "Product A" has a price of 100')
+    console.log("Question 4C: ", 'No, according to the provided JSON object, the launch date of "Product B" is "2010-06-01T00:00:00.000Z," which corresponds to the 1st of June 2010.')
+    console.log("Question 5: ", 'No, according to the provided JSON object, the launch date of "Product B" is "2010-06-01T00:00:00.000Z," which corresponds to the 1st of June 2010.')
+
+    const effectiveDate = new Date(apiData?.DataProvider?.effectiveDate);
+    const formattedDate = `${effectiveDate.getFullYear()}-${effectiveDate.toLocaleString('default', { month: 'short' })}-${effectiveDate.getDate()}`;
+    console.log("Question 5: ", 'Formatted effective date:', formattedDate);
+    console.log("Question 6: ", 'DataProvider.global.positions[2].profile.email');
+
+    const positionHolders = apiData?.DataProvider.global.positions;
+    const uniqueEmailDomains = [...new Set(positionHolders.map(holder => holder.profile.email.split('@')[1]))];
+
+    console.log('Question 7: ', uniqueEmailDomains);
+    console.log("Question 8: ", apiData?.DataProvider?.global?.positions?.filter(position => position.type === 'DIRECTOR').map(director => ({ name: director.profile.name, email: director.profile.email })));
+
+
+    function getPositionHoldersByDomain(emailDomain) {
+        const positionHolders = apiData?.DataProvider.global.positions;
+        const filteredPositionHolders = positionHolders.filter(holder => holder.profile.email.endsWith(`@${emailDomain}`));
+        const positionHolderDetails = filteredPositionHolders.map(holder => ({ name: holder.profile.name, email: holder.profile.email, _id: holder._id }));
+
+        return positionHolderDetails;
+    }
+
+    // Example usage:
+    const emailDomain = 'example.com';
+    // const positionHolders = getPositionHoldersByDomain(emailDomain);
+    console.log("Question 9: ", getPositionHoldersByDomain(emailDomain));
+
+
+    function getTopShareholders() {
+        const positionHolders = apiData?.DataProvider.global.positions;
+        const shareholdings = apiData?.DataProvider.shareholdings;
+
+        const shareholderDetails = positionHolders
+            .filter(holder => holder.type === 'SHAREHOLDER' && holder.profile.email.endsWith('example.com'))
+            .map((holder, index) => ({
+                name: holder.profile.name,
+                email: holder.profile.email,
+                shares: shareholdings[index]
+            }))
+            .sort((a, b) => b.shares - a.shares)
+            .slice(0, 3); //first 3
+
+        return shareholderDetails;
+    }
+
+    // Example usage:
+    const topShareholders = getTopShareholders();
+    console.log("Question 10: ", getTopShareholders());
+
+
     return (
         <div className="PARENT text-white text-4xl">
-            hahahh
+            dsf
         </div>
     )
 }
